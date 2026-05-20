03b38f15f814bf6707de9a8f00b272fe900f16ff
developer <17201665342@163.com>
2026-05-20 21:30:21 +0800

    V5.20

--- pages/product_page.py
+++ pages/product_page.py
@@ -8,20 +8,40 @@ from config.settings import settings_frontend
 class ProductPage(BasePage):
     url = settings_frontend.config["base_url"]
 
-    product_view = (By.XPATH, "//a[contains(@href, '/view/1')]")
+    product_create_button = (By.LINK_TEXT, "New Product")
+    product_view1 = (By.XPATH, "//a[contains(@href, '/view/1')]")
+    product_view2 = (By.XPATH, "//a[contains(@href, '/view/2')]")
     view1_products = (By.LINK_TEXT, "Products")
     view1_edit = (By.XPATH, "//a[contains(@href, '/edit/1')]")
+    view2_edit = (By.XPATH, "//a[contains(@href, '/edit/2')]")
     product_name_input = (By.ID, "id_name")
     product_description_input = (By.ID, "id_description")
     product_price_input = (By.ID, "id_price")
+    update_button = (By.XPATH, "//button[contains(@type, 'submit')]")
 
     def open(self):
         self.driver.get(self.url)
 
-    def is_view_success(self):
+    def is_click_create_product_button_success(self):
+        try:
+            WebDriverWait(self.driver, 5).until(EC.url_contains("/new"))
+            return True
+        except:
+            return False
+
+    def is_view1_success(self):
         try:
             WebDriverWait(self.driver, 10).until(
                 EC.url_contains("/view/1")
+            )
+            return True
+        except:
+            return False
+
+    def is_view2_success(self):
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.url_contains("/view/2")
             )
             return True
         except:
@@ -36,7 +56,30 @@ class ProductPage(BasePage):
         except:
             return False
 
+    def is_access_edit_product2_success(self):
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.url_contains("/edit/2")
+            )
+            return True
+        except:
+            return False
+
+    def clean_inputs(self):
+        self.find(self.product_name_input).clear()
+        self.find(self.product_description_input).clear()
+        self.find(self.product_price_input).clear()
+
     def input_product_details(self, name, description, price):
         self.input(self.product_name_input, name)
         self.input(self.product_description_input, description)
         self.input(self.product_price_input, price)
+
+    def is_update_product_success(self):
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.text_to_be_present_in_element((By.CLASS_NAME, "alert-success"), "Product successfully updated!")
+            )
+            return True
+        except:
+            return False
--- tests/ui/test_product.py
+++ tests/ui/test_product.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 """ 项目模块 """
 
 
-def test_product_create_button(driver, expected_success = True):
+def test_product_create_button(driver, general_login, expected_success = True):
     page = ProductPage(driver)
     page.open()
     page.click(page.product_create_button)
@@ -18,24 +18,25 @@ def test_product_create_button(driver, expected_success = True):
 @pytest.mark.parametrize("name, description, price, expected_success", [
     ("Test Product1", "This is a test product.", "2", True),
 ])
-def test_product_view(driver, general_login, expected_success, name, description, price):
+def test_product2_view(driver, general_login, expected_success, name, description, price):
     page = ProductPage(driver)
     page.open()
     pause(2)
-    page.click(page.product_view)
-    pause(2)
+    page.click(page.product_view2)
 
-    assert page.is_view_success() == expected_success
+    assert page.is_view2_success() == expected_success
 
-    page.click(page.view1_products)
-    pause(2)
+    page.click(page.view2_edit)
 
-    assert page.is_return_products_main_page() == expected_success
+    assert page.is_access_edit_product2_success() == expected_success
 
-    page.click(page.product_view)
-    page.click(page.view1_edit)
+    page.clean_inputs()
     page.input_product_details(name, description, price)
-    pause(5)
+    pause(1)
+    page.click(page.update_button)
+    pause(2)
+
+    assert page.is_update_product_success() == expected_success
 
 
 
